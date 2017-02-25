bf3e8cc18be60037ed1bf36ae479915213ac91c1
Paul Romine <promine@gmail.com>
2017-02-25 14:02:01 -0600

    start working on #17 - letter character counting

---
+++ numberLetterCount.py
@@ -0,0 +1,26 @@
+import math
+
+
+def lettersPerOnesPlace(number):
+    if number==1:
+        return 3
+    if number==2:
+        return 3
+    if number==3:
+        return 5
+    if number==4:
+        return 4
+    if number==5:
+        return 4
+    if number==6:
+        return 3
+    if number==7:
+        return 5
+    if number==8:
+        return 5
+    if number==9:
+        return 4
+    if number==0:
+        return 4
+
+def lettersPerTensPlace(number):
