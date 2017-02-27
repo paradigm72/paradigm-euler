74df1628d3b6d045a885c00d4d808e4c31cb5544
Paul Romine <promine@gmail.com>
2017-02-26 22:08:01 -0600

    stitching togethers tens and hundreds places

--- numberLetterCount.py
+++ numberLetterCount.py
@@ -2,25 +2,67 @@ import math
 
 
 def lettersPerOnesPlace(number):
-    if number==1:
+    if number == 1:
         return 3
-    if number==2:
+    if number == 2:
         return 3
-    if number==3:
+    if number == 3:
         return 5
-    if number==4:
+    if number == 4:
         return 4
-    if number==5:
+    if number == 5:
         return 4
-    if number==6:
+    if number == 6:
         return 3
-    if number==7:
+    if number == 7:
         return 5
-    if number==8:
+    if number == 8:
         return 5
-    if number==9:
+    if number == 9:
         return 4
-    if number==0:
+    if number == 0:
         return 4
 
-def lettersPerTensPlace(number):
+
+def lettersPerTensPlace(tens, ones, appendOnesLetters):
+    if tens == 1:
+        if ones == 1:
+            appendOnesLetters[0] = False
+            return 6     #eleven
+        if ones == 2:
+            appendOnesLetters[0] = False
+            return 6     #twelve
+        if ones == 3:
+            appendOnesLetters[0] = False
+            return 8     #thirteen
+        else:
+            return 4     # "teen"
+    if tens == 2:
+        return 6     #twenty
+    if tens == 3:
+        return 6     #thirty
+    if tens == 4:
+        return 6     #fourty
+    if tens == 5:
+        return 5     #fifty
+    else:
+        return lettersPerOnesPlace(tens) + 2  #<onesplace>ty
+
+
+
+def lettersPerNumber(number):
+    ones = number % 10
+    tens = (number - ones) % 100
+    hundreds = number - tens - ones
+
+    appendOnesLetters = [True]
+    hLetters = lettersPerOnesPlace(hundreds)
+    tLetters = lettersPerTensPlace(tens,ones,appendOnesLetters)
+    oLetters = lettersPerOnesPlace(ones)
+    if hLetters > 0:
+        hLetters += 10  #hundred and
+
+    totalLetters = hLetters + tLetters + oLetters
+    print "Letters for number ",number,": ",totalLetters,"."
+
+ lettersPerNumber(142)
