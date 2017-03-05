5af39568146f7c074b310c4247de1d2599cc8113
Paul Romine <promine@gmail.com>
2017-03-04 23:06:11 -0600

    fix bugs with fifteen/ten, still some remaining

--- numberLetterCount.py
+++ numberLetterCount.py
@@ -21,7 +21,7 @@ def lettersPerOnesPlace(number):
     if number == 9:
         return 4
     if number == 0:
-        return 4
+        return 0   #we never actually say zero
 
 
 def lettersPerTensPlace(tens, ones, appendOnesLetters):
@@ -35,6 +35,12 @@ def lettersPerTensPlace(tens, ones, appendOnesLetters):
         if ones == 3:
             appendOnesLetters[0] = False
             return 8     #thirteen
+        if ones == 5:
+            appendOnesLetters[0] = False
+            return 7     #fifteen
+        if ones == 0:
+            appendOnesLetters[0] = False
+            return 3     #ten
         else:
             return 4     # "teen"
     if tens == 2:
@@ -55,9 +61,20 @@ def lettersPerNumber(number):
     tens = (number - ones) % 100
     hundreds = number - tens - ones
 
+    if number == 10:
+        junk = 1
+
     appendOnesLetters = [True]
-    hLetters = lettersPerOnesPlace(hundreds // 100)
-    tLetters = lettersPerTensPlace(tens // 10,ones,appendOnesLetters)
+    if hundreds > 0:
+        hLetters = lettersPerOnesPlace(hundreds // 100)
+    else:
+        hLetters = 0
+
+    if tens > 0:
+        tLetters = lettersPerTensPlace(tens // 10,ones,appendOnesLetters)
+    else:
+        tLetters = 0
+
     if appendOnesLetters[0]:
         oLetters = lettersPerOnesPlace(ones)
     else:
@@ -67,10 +84,15 @@ def lettersPerNumber(number):
         hLetters += 10  #"hundred and"
 
     totalLetters = hLetters + tLetters + oLetters
-    print "Letters for number ",number,": ",totalLetters,"."
+    return totalLetters
 
 
-lettersPerNumber(142)
-print "One hundred and forty-two"
-lettersPerNumber(712)
-print "Seven hundred and twelve"
+def lettersForAllNumbersUpTo(finalNumber):
+    runningTotal = 0
+    for i in range(1,finalNumber):
+        runningTotal += lettersPerNumber(i)
+        print i," has ",lettersPerNumber(i)," letters."
+
+    print "Total number of letters up to ",finalNumber,": ",runningTotal
+
+lettersForAllNumbersUpTo(1000)
