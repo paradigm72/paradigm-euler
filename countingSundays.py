e7d3b080ee037c28077799108a57cac192c166d1
Paul Romine <promine@gmail.com>
2022-08-06 21:43:28 -0500

    start on lib for #19

---
+++ countingSundays.py
@@ -0,0 +1,7 @@
+from lib import calendar
+
+print "Was 1977 a leap year? ", calendar.isLeapYear(1977)
+print "Was 1996 a leap year? ", calendar.isLeapYear(1996)
+print "Was 2000 a leap year? ", calendar.isLeapYear(2000)
+print "Was 1900 a leap year? ", calendar.isLeapYear(1900)
+
--- powersOfTwo.py
+++ powersOfTwo.py
@@ -1,10 +1,10 @@
-import atomicOperators
-import time
+from lib import atomicOperators
+
 
 def printExponents(base,maxExponent):
     currentVal = base
     for i in range(1,maxExponent+1):
-        print  i,"  ",currentVal,"  ",atomicOperators.sumDigits(currentVal,10)
+        print  i,"  ",currentVal,"  ", atomicOperators.sumDigits(currentVal, 10)
         currentVal = currentVal * base
 
 
@@ -17,4 +17,4 @@ def getFinalMultiple(base,maxExponent):
 #printExponents(2,1000);
 #print getFinalMultiple(2,1000);
 #print atomicOperators.sumDigits(21430172143725346418968500981200036211228096234110672148875007767407021022498722449863967576313917162551893458351062936503742905713846280871969155149397149607869135549648461970842149210124742283755908364306092949967163882534797535118331087892154125829142392955373084335320859663305248773674411336138752,10);
-print atomicOperators.sumDigits(getFinalMultiple(2,1000),10);
+print atomicOperators.sumDigits(getFinalMultiple(2, 1000), 10);
