01efe02ad09d6689511df1b51335c36225548462
Paul Romine <promine@gmail.com>
2022-08-08 07:02:24 -0500

    add calc for days in month

--- lib/calendar.py
+++ lib/calendar.py
@@ -6,3 +6,14 @@ def isLeapYear(year):
             return False;
         return True;
     return False;
+
+
+def daysInMonth(month, year):
+    if (month == 1) or (month == 3) or (month == 5) or (month == 7) or (month == 8) or (month == 10) or (month == 12):
+        return 31
+    if month == 2:
+        if isLeapYear(year):
+            return 29
+        return 28
+    return 30
+
--- test/calendarSpec.py
+++ test/calendarSpec.py
@@ -2,14 +2,19 @@ import unittest
 from lib import calendar
 
 
-class MyTestCase(unittest.TestCase):
-    def test_leap_years(self):
+class CalendarTests(unittest.TestCase):
+    def testIsLeapYear(self):
         self.assertEqual(calendar.isLeapYear(1977), False)
         self.assertEqual(calendar.isLeapYear(1980), True)
         self.assertEqual(calendar.isLeapYear(1900), False)
         self.assertEqual(calendar.isLeapYear(2000), True)
         self.assertEqual(calendar.isLeapYear(6723), False)
 
+    def testDaysInMonth(self):
+        self.assertEqual(calendar.daysInMonth(1, 1974), 31)
+        self.assertEqual(calendar.daysInMonth(2, 1980), 29)
+        self.assertEqual(calendar.daysInMonth(2, 1981), 28)
+        self.assertEqual(calendar.daysInMonth(4, 1981), 30)
 
 if __name__ == '__main__':
     unittest.main()
