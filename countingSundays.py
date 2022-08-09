37be5349b66c3d344133ab2463de6bc6664e1e33
Paul Romine <promine@gmail.com>
2022-08-09 15:41:45 -0500

    refactor daysInYear, sketch main loop

--- countingSundays.py
+++ countingSundays.py
@@ -1,7 +1,13 @@
 from lib import calendar
 
-print "Was 1977 a leap year? ", calendar.isLeapYear(1977)
-print "Was 1996 a leap year? ", calendar.isLeapYear(1996)
-print "Was 2000 a leap year? ", calendar.isLeapYear(2000)
-print "Was 1900 a leap year? ", calendar.isLeapYear(1900)
+
+def loopAllDaysInCentury():
+    dayInWeek = 1
+    # get the list of first days of each month, by day-in-year number
+    for year in range(1901, 2000):
+        for dayInYear in range(1, calendar.daysInYear(year)):
+            # if this is a Sunday, check whether it's in the list
+            # if so, increment counter and reset dayInWeek to 1
+            # otherwise increment dayInWeek = dayInWeek + 1
+            pass
 
--- lib/calendar.py
+++ lib/calendar.py
@@ -8,6 +8,10 @@ def isLeapYear(year):
     return False;
 
 
+def daysInYear(year):
+    return 366 if isLeapYear(year) else 365
+
+
 def daysInMonth(month, year):
     if (month == 1) or (month == 3) or (month == 5) or (month == 7) or (month == 8) or (month == 10) or (month == 12):
         return 31
@@ -21,10 +25,9 @@ def daysInMonth(month, year):
 def getFirstDaysInMonth(year):
     dayNumInMonth = 0
     monthNum = 1
-    dayNums = [1]   # start with Jan. 1 as a first day of a month
-    daysInYear = 366 if isLeapYear(year) else 365
+    dayNums = [1]   # start with Jan. 1 as a given first day of a month
 
-    for dayNumInYear in range(1, daysInYear):
+    for dayNumInYear in range(1, daysInYear(year)):
         dayNumInMonth = dayNumInMonth + 1
         if dayNumInMonth > daysInMonth(monthNum, year):
             monthNum = monthNum + 1
