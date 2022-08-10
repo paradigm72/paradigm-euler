04c46999cc185e059a7fa9a412a96019fc8c843d
Paul Romine <promine@gmail.com>
2022-08-10 07:16:25 -0500

    full algorithm
    but answer isn't right, maybe it's 1 too often?

--- countingSundays.py
+++ countingSundays.py
@@ -3,11 +3,18 @@ from lib import calendar
 
 def loopAllDaysInCentury():
     dayInWeek = 1
-    # get the list of first days of each month, by day-in-year number
-    for year in range(1901, 2000):
+    firstSundays = 0
+    for year in range(1901, 2001):
+        firstDaysInMonth = calendar.getFirstDaysInMonth(year)
         for dayInYear in range(1, calendar.daysInYear(year)):
-            # if this is a Sunday, check whether it's in the list
-            # if so, increment counter and reset dayInWeek to 1
-            # otherwise increment dayInWeek = dayInWeek + 1
-            pass
+            if dayInWeek == 7:
+                if dayInYear in firstDaysInMonth:
+                    firstSundays = firstSundays + 1
+                dayInWeek = 1
+            else:
+                dayInWeek = dayInWeek + 1
+        print "As of", year, ", count is", firstSundays
+    print "Final count of first Sundays:", firstSundays
 
+
+loopAllDaysInCentury()
