a0a99e9b3a0f70a370efe6ffab8d2f66ee873796
Paul Romine <promine@gmail.com>
2022-08-11 07:18:58 -0500

    fix off by one
    and add some tracing

--- countingSundays.py
+++ countingSundays.py
@@ -6,10 +6,13 @@ def loopAllDaysInCentury():
     firstSundays = 0
     for year in range(1901, 2001):
         firstDaysInMonth = calendar.getFirstDaysInMonth(year)
-        for dayInYear in range(1, calendar.daysInYear(year)):
+        for dayInYear in range(1, calendar.daysInYear(year) + 1):
             if dayInWeek == 7:
                 if dayInYear in firstDaysInMonth:
                     firstSundays = firstSundays + 1
+                    print year, "Sunday, day number", dayInYear, "was a first of the month"
+                else:
+                    print year, "Sunday, day number", dayInYear, "was not a first of the month"
                 dayInWeek = 1
             else:
                 dayInWeek = dayInWeek + 1
